123d89bab7c44da70bd9a913987d771c9d48d982
CptCaveira <janos.rocha@gmail.com>
2021-06-28 12:13:07 +0200

    dataloader file

--- projectscout/contractrequest.py
+++ projectscout/contractrequest.py
@@ -1,4 +1,8 @@
 import requests
-
-response = requests.get('https://www.capology.com/api/v2/soccer/contracts/es/la-liga/2020-2021/')
-print(response.json())
+import json
+url = 'https://www.capology.com/api/v2/soccer/payrolls/de/1-bundesliga/2020-2021/'
+headers = {"x-api-key": "828051bb8fcc41189822a19249477ae5"}
+r = requests.get(url, headers=headers)
+file = r.json()
+with open('file.json', 'w') as f:
+    json.dump(file, f,indent="")
