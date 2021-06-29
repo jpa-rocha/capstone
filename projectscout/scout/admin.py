4af6b429779b70025f6de56364ddcfed7d22fbc5
CptCaveira <janos.rocha@gmail.com>
2021-06-29 10:07:17 +0200

    ordering changes

--- projectscout/scout/admin.py
+++ projectscout/scout/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from .models import League, MiscStats, Team, Player, PlayingTime
+from .models import MiscStats, League, Player, PlayingTime, ShootingStats, Team, AerialDuels, PossessionStats, PassingStats, PassTypesStats, DefensiveStats, GoalShotCreationStats, GoalkeepingStats
 # Register your models here.
 admin.site.register(League)
 admin.site.register(Team)
 admin.site.register(Player)
 admin.site.register(PlayingTime)
 admin.site.register(MiscStats)
+admin.site.register(ShootingStats)
+admin.site.register(AerialDuels)
+admin.site.register(PossessionStats)
+admin.site.register(PassingStats)
+admin.site.register(PassTypesStats)
+admin.site.register(DefensiveStats)
+admin.site.register(GoalShotCreationStats)
+admin.site.register(GoalkeepingStats)
