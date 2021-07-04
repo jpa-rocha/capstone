43003abf75a032497347b1183396add1439f556f
João Rocha <71012028+CptCaveira@users.noreply.github.com>
2021-07-04 13:54:30 +0200

    Delete contractrequest.py

--- projectscout/contractrequest.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-url = 'https://www.capology.com/api/v2/soccer/payrolls/de/1-bundesliga/2020-2021/'
-headers = {"x-api-key": "828051bb8fcc41189822a19249477ae5"}
-r = requests.get(url, headers=headers)
-file = r.json()
-with open('file.json', 'w') as f:
-    json.dump(file, f,indent="")
