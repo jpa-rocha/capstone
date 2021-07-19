76cb2c5b8212126143729d23336e1ed909cabbd4
CptCaveira <janos.rocha@gmail.com>
2021-07-19 13:56:55 +0200

    salary graph finished

--- projectscout/scout/static/scout/team.js
+++ projectscout/scout/static/scout/team.js
@@ -6,60 +6,67 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 function salaryoverview(){
-    var minreservesalary = parseFloat(document.getElementById('minreservesalary').innerText)
-    var minstartersalary = parseFloat(document.getElementById('minstartersalary').innerText)
-    var mintotalsalary = parseFloat(document.getElementById('mintotalsalary').innerText)
 
-    var percentile25startersalary = parseFloat(document.getElementById('percentile25startersalary').innerText)
-    var percentile25reservesalary = parseFloat(document.getElementById('percentile25reservesalary').innerText)
-    var percentile25totalsalary = parseFloat(document.getElementById('percentile25totalsalary').innerText)
-
-    var medianreservesalary = parseFloat(document.getElementById('medianreservesalary').innerText)
-    var medianstartersalary = parseFloat(document.getElementById('medianstartersalary').innerText)
-    var mediantotalsalary = parseFloat(document.getElementById('mediantotalsalary').innerText)
-
-    var percentile75reservesalary = parseFloat(document.getElementById('percentile75reservesalary').innerText)
-    var percentile75startersalary = parseFloat(document.getElementById('percentile75startersalary').innerText)
-    var percentile75totalsalary = parseFloat(document.getElementById('percentile75totalsalary').innerText)
-
-    var maxreservesalary = parseFloat(document.getElementById('maxreservesalary').innerText)
-    var maxstartersalary = parseFloat(document.getElementById('maxstartersalary').innerText)
-    var maxtotalsalary = parseFloat(document.getElementById('maxtotalsalary').innerText)
-
-    var teamreservesalary = document.getElementById('teamreservesalary').innerText.replace('€','')
-    teamreservesalary = parseFloat(teamreservesalary.replace(',','')) 
-    var teamstartersalary = document.getElementById('teamstartersalary').innerText.replace('€','')
-    teamstartersalary = parseFloat(teamstartersalary.replace(',',''))
-    var totalteamsalary = document.getElementById('totalteamsalary').innerText.replace('€','')
-    totalteamsalary = parseFloat(totalteamsalary.replace(',',''))
+    var teamname = document.getElementById('teamname').innerText;
     
-    var trace1 = {
-        x: ['Lowest', '25th Percentile', 'Median', '75th percentile', 'Highest'],
-        y: [minstartersalary, percentile25startersalary, medianstartersalary, percentile75startersalary, maxstartersalary],
-        name: 'Starters Salary',
-        type: 'bar'
-      };
-      
-    var trace2 = {
-        x: ['Lowest', '25th Percentile', 'Median', '75th percentile', 'Highest'],
-        y: [minreservesalary, percentile25reservesalary, medianreservesalary, percentile75reservesalary, maxreservesalary],
-        name: 'Reserves Salary',
-        type: 'bar'
-    };
+    var dictreservesalary = document.getElementById('dictreservesalary').innerText;
+    var dictstartersalary = document.getElementById('dictstartersalary').innerText;
+    var dicttotalsalary = document.getElementById('dicttotalsalary').innerText;
+
+    var reservesalary = JSON.parse(dictreservesalary);
+    var startersalary = JSON.parse(dictstartersalary);
+    var totalsalary = JSON.parse(dicttotalsalary);
     
+
+    var keysreserves = [];
+    var valuesreserves = [];
+    for (key in reservesalary){
+        keysreserves.push(reservesalary[key][0])
+        valuesreserves.push(reservesalary[key][1])
+    }
+    var keysstarters = [];
+    var valuesstarters = [];
+    for (key in startersalary){
+        keysstarters.push(startersalary[key][0])
+        valuesstarters.push(startersalary[key][1])
+    }
+    var keystotal = [];
+    var valuestotal = [];
+    for (key in totalsalary){
+        keystotal.push(totalsalary[key][0])
+        valuestotal.push(totalsalary[key][1])
+    }
+
+    console.log(valuestotal)
     var trace3 = {
-        x: ['Lowest', '25th Percentile', 'Median', '75th percentile', 'Highest'],
-        y: [mintotalsalary, percentile25totalsalary, mediantotalsalary, percentile75totalsalary, maxtotalsalary],
-        name: 'Total Salary',
-        type: 'bar'
+        x : keysreserves,
+        y : valuesreserves,
+        type : 'bar',
+        name : 'Reserves Salaries'
     };
-    var data = [trace1, trace2, trace3];
 
-    var layout = {title: 'League Salary Distributions',
-                  barmode: 'group'};
+    var trace2 = {
+        x : keysstarters,
+        y : valuesstarters,
+        type : 'bar',
+        name : 'Starters Salaries'
+    };
+
+    var trace1 = {
+        x : keystotal,
+        y : valuestotal,
+        type : 'bar',
+        name : 'Total Salaries'
+    };
+
+    var data = [trace1, trace2, trace3];
+    var layout = {barmode: 'group'};
 
     Plotly.newPlot('salaryoverview', data, layout,{scrollZoom: true, 
-                                                   displayModeBar: false});
+                                                   displayModeBar: false,
+                                                   responsive: true,
+                                                   showlegend: true,
+                                                   });
 }
 
 
@@ -72,14 +79,17 @@ function get_team(name){
             if (stat['name'] != undefined){
            playernames.push(stat['name'])
             }
-        })
+        });
+        
         playernames.forEach(player=>{
             statslist.forEach(stat=>{
                 if (stat['name'] == player || stat['player'] == player){
+                    var keys = Object.keys(stat)
+                    
                     
                 }
-            })
-        })
+            });
+        });
         
     })
     .catch(error => console.log('error:', error));
